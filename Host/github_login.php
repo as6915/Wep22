538603900fe5956fc5018ff28c9d34fc1da28260
as6915 <alfkhmbdalrhmnalastwrh@gmail.com>
2024-09-03 18:19:37 +0300

    Delete Host directory

--- Host/github_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$client_id = 'وهنا برضو id';
-$redirect_uri = 'http://yourdomain.com/github_callback.php';
-$scope = 'user';
-header("Location: https://github.com/login/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&scope=$scope");
-exit();
-?>

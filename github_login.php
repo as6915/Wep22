2a486a7d8e6dba69f88401116a29a57ef229f719
as6915 <alfkhmbdalrhmnalastwrh@gmail.com>
2024-09-03 18:40:12 +0300

    Update github_login.php

--- github_login.php
+++ github_login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$client_id = 'وهنا برضو id';
+$client_id = 'Iv23li3Mt1ZwYvSLvb9h';
 $redirect_uri = 'http://yourdomain.com/github_callback.php';
 $scope = 'user';
 header("Location: https://github.com/login/oauth/authorize?client_id=$client_id&redirect_uri=$redirect_uri&scope=$scope");
